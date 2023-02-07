8863f37b2d9b7eba049f4b225a8994c7032877eb
emkjee <123936268+emkjee@users.noreply.github.com>
2023-02-07 13:27:08 +0530

    Symlink non interactive shell to homebrew installed shell.. but its not possible.. so tried to symlink it to the default zsh shell. !!!

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-echo "\n<<< Starting ZSH Setup >>>\n"
+echo "\n|%-> Starting ZSH Setup <-%|\n"
 
 #how-to-test-if-string-exists-in-file-with-bash/4749368#4749368
 #https://stackoverflow.com/questions/4749330 how-to-test-if-string-exists-in-file-with-bash/4749368#4749368
@@ -9,7 +9,7 @@ if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
   echo '|%->'
 else
   echo '|%-> Enter superuser (sudo) password to edit /etc/shells'
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
@@ -17,5 +17,16 @@ if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
   echo '|%->'
 else
   echo '|%-> Enter user password to change login shell'
-chsh -s '/usr/local/bin/zsh'	
+  chsh -s '/usr/local/bin/zsh'
 fi
+
+# if sh --version | grep -q zsh; then
+#   echo '/private/var/select/sh already linked to /bin/zsh'
+#   echo ' '
+# else
+#   echo 'Enter superuser (sudo) password to symlink sh to zsh'
+#   sudo ln -sfv /bin/zsh /private/var/select/sh
+
+#   #I would like this one to work instead!
+#   #sudo ln -sfv /usr/local/bin/zsh /private/var/select/sh
+# fi
