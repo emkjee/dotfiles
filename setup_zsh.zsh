5c9f93d1d8b4bb09b6e12d0d5e32d80a3535e36b
emkjee <123936268+emkjee@users.noreply.github.com>
2023-02-07 13:24:41 +0530

    Prevent extraneous appending to /etc/shells file.

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,10 +2,15 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-# Installation unnecessary; it is in the Brewfile.
-
-echo "Enter superuser (sudo) password to edit /etc/shells" 
+#how-to-test-if-string-exists-in-file-with-bash/4749368#4749368
+#https://stackoverflow.com/questions/4749330 how-to-test-if-string-exists-in-file-with-bash/4749368#4749368
+if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
+  echo '|%-> /usr/local/bin/zsh already exists in /etc/shells'
+  echo '|%->'
+else
+  echo '|%-> Enter superuser (sudo) password to edit /etc/shells'
 echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
 echo "enter user password to change login shell"
 chsh -s '/usr/local/bin/zsh'	
