ad85abc74684a7074f3c719bb9f3742af2eb1a39
emkjee <123936268+emkjee@users.noreply.github.com>
2025-02-26 18:03:22 +0530

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+stow --target ~/ --dir . git
+stow --target ~/ --dir . helix
+stow --target ~/ --dir . starship
+stow --target ~/ --dir . shell
+stow --target ~/Library/Application\ Support/Code/User --dir . vscode
