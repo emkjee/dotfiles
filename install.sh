2ffd1a3bca23efece120f680ad37ec175a4e7b68
emkjee <123936268+emkjee@users.noreply.github.com>
2025-04-21 15:57:05 +0530

    Added bat

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 stow --target ~/ --dir . git
 stow --target ~/ --dir . helix
 stow --target ~/ --dir . starship
+stow --target ~/ --dir . bat
 stow --target ~/ --dir . shell
 stow --target ~/Library/Application\ Support/Code/User --dir . vscode
