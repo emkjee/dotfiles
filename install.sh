5aef62bc75c817fb1b9cdc5251fb83ad2e6605c6
emkjee <123936268+emkjee@users.noreply.github.com>
2025-07-30 17:05:30 +0530

    Modify install.sh -> Added kitty & ruff

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-stow --dotfiles --target ~/ --dir . git
-stow --dotfiles --target ~/ --dir . helix
-stow --dotfiles --target ~/ --dir . starship
-stow --dotfiles --target ~/ --dir . bat
-stow --dotfiles --target ~/ --dir . shell
-stow --dotfiles --target ~/Library/Application\ Support/Code/User --dir . vscode
+stow -R --dotfiles --target ~/ --dir . git
+stow -R --dotfiles --target ~/ --dir . helix
+stow -R --dotfiles --target ~/ --dir . starship
+stow -R --dotfiles --target ~/ --dir . kitty
+stow -R --dotfiles --target ~/ --dir . bat
+stow -R --dotfiles --target ~/ --dir . shell
+stow -R --dotfiles --target ~/ --dir . ruff
+stow -R --dotfiles --target ~/Library/Application\ Support/Code/User --dir . vscode
